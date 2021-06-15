b8bd0a8cb771ca6a66ba5ac124f2a2021052b973
Draiget <draiget@gmail.com>
2021-06-15 11:49:50 +0400

    Initial commit

---
+++ mod_info.lua
@@ -0,0 +1,10 @@
+name = "Selection Cost UI"
+version = 6
+copyright = ""
+description = "Displays cost information for currently selected units.  No longer displays cost of ACU.  Rounds mass and e values.  Also displays the current spend rate of the selected units."
+author = "Blodir, Softly, Washy"
+url = ""
+uid = "2018eaac-e8ed-11e4-b02c-1681e6b88ec6"
+
+exclusive = false
+ui_only = true
